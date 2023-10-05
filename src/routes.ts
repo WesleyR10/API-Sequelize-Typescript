8dd43480a46639346cfbc8e47d3fa509581fc24a
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-05 17:57:30 -0300

    refactoring routes

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import { Candidate } from './models/candidate'
+
+const router = express.Router()
+
+router.get('/', (req, res) => res.json({ hello: 'Hello, world!' }))
+
+//Rota que retorna os candidatos
+router.get('/candidates', async (req, res) => {
+    const candidates = await Candidate.findAll()
+    return res.json(candidates)
+})
+
+export { router }
