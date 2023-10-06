5dc4bfb0af26ab2f6778bcfd832307934def6a74
WesleyR10 <wesleyribas2015@gmail.com>
2023-10-05 21:09:24 -0300

    continuation of company edpoints

--- src/Controllers/companies-controller.ts
+++ src/Controllers/companies-controller.ts
@@ -10,7 +10,75 @@ const companiesController = {
             return res.json(companies)
         } catch (err) {
             if (err instanceof Error) {
-								return res.status(400).json({ message: err.message })
+				return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+  // Metodo que insere companies - POST - /companies
+    save: async (req: Request, res: Response) => {
+        const { name, bio, website ,email} = req.body // 
+
+        try {
+        const company = await Company.create({ name, bio, website ,email,})
+            return res.status(201).json(company)
+    } catch (err) {
+        if (err instanceof Error) {
+            return res.status(400).json({ message: err.message })
+        }
+    }},
+
+  // Metodo GET /companies/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            const company = await Company.findByPk(id)
+            return res.json(company)
+        } catch (err) {
+            if (err instanceof Error) {
+				return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+  // Metodo para atualizar - PUT - /companies/:id
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params
+        const { name, bio, website, email } = req.body
+
+        try {
+            const [affectedRows, companies] = await Company.update({ // Metodo update do sequelize - primeiro os valores para att
+                name,
+                bio,
+                website,
+                email,
+            }, { // Segundo passar as opções 
+                where: { id },
+                returning: true // Somente para o postgres - retorna as linhas afetadas
+            })
+    
+            return res.json(companies[0])
+        } catch (err) {
+            if (err instanceof Error) {
+				return res.status(400).json({ message: err.message })
+            }
+        }
+    },
+
+  // DELETE /companies/:id
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        try {
+            await Company.destroy({
+                where: { id }
+            })
+    
+            return res.status(204).send()
+        } catch (err) {
+            if (err instanceof Error) {
+				return res.status(400).json({ message: err.message })
             }
         }
     }
--- src/routes.ts
+++ src/routes.ts
@@ -11,10 +11,14 @@ router.get('/', (req, res) => res.json({ hello: 'Hello, world!' }))
   router.get('/candidates/:id', candidatesController.show) //Rota que retorna um candidato especifico
   router.post('/candidates', candidatesController.save) //Rota que insere uma informação no banco de dados
   router.put('/candidates/:id', candidatesController.update) //Rota que atualiza os dados no banco de dados
-  router.delete('/candidates/:id', candidatesController.delete) //Rota que atualiza os dados no banco de dados
+  router.delete('/candidates/:id', candidatesController.delete) //Rota que deleta um candidato do banco de dados
   
   // Todas as rotas de companies
-  router.get('/companies', companiesController.index)
+  router.get('/companies', companiesController.index) //Rota que retorna todos as empresas
+  router.get('/companies/:id', companiesController.show) //Rota que retorna uma empresa especifica
+  router.post('/companies', companiesController.save) //Rota que insere uma informação no banco de dados
+  router.put('/companies/:id', companiesController.update) //Rota que atualiza os dados de uma company
+  router.delete('/companies/:id', companiesController.delete) //Rota que deleta uma company do banco de dados
 
 
 export { router }
